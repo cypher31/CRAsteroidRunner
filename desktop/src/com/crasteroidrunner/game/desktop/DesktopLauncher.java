34da54c1f81b328ac3858e5744f152787c71ef67
cypher31 <kelbystyler@gmail.com>
2015-12-16 17:03:12 -0800

    Started project
    Used libGDX java app

---
+++ desktop/src/com/crasteroidrunner/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.crasteroidrunner.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crasteroidrunner.game.CaptainRickAR;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CaptainRickAR(), config);
+	}
+}
