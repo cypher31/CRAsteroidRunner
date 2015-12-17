eef15a8e02f4b2b4d7b26e115ce9fcac80933d8d
cypher31 <kelbystyler@gmail.com>
2015-12-16 17:34:31 -0800

    Fooling around

--- core/src/com/crasteroidrunner/game/CaptainRickAR.java
+++ core/src/com/crasteroidrunner/game/CaptainRickAR.java
@@ -9,19 +9,32 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CaptainRickAR extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private static float WORLD_TO_SCREEN = 1.0f / 1.0f;
+	
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		
 	}
 
 	@Override
 	public void render () {
+		int w = img.getWidth();
+		int h = img.getHeight();
+		
+		float xOrigin = w * .5f;
+		float yOrigin = h * .5f;
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//I think this doesnt work the correct way because we need to have a camera so we can do the projection matrix.
+		batch.draw(img, Gdx.graphics.getWidth() / 2 - xOrigin, Gdx.graphics.getHeight() / 2 - yOrigin, xOrigin, yOrigin, w, h, WORLD_TO_SCREEN, WORLD_TO_SCREEN, 
+				0.0f, 0, 0, w, h, false, false);
 		batch.end();
 	}
 }
